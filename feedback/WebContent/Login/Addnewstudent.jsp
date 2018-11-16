86b7c40a2db510a6f7834622aba9b6339f9dcf8d
sreerag <sreeragabc1998@gmail.com>
2018-11-16 12:00:09 +0530

    new commits

---
+++ feedback/WebContent/Login/Addnewstudent.jsp
@@ -0,0 +1,80 @@
+<%@ page import="java.util.List"%>
+<%@ page import="java.util.ArrayList"%>
+<%@ page import="com.feedback.model.ClassModel"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FeedBack</title>
+        <link rel="stylesheet" href="../css/normalize.css">
+        <link href='https://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="../css/main2.css">
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<script type="text/javascript">
+	function validate(addnewteacher) {
+
+		var pass = document.getElementById("password").value;
+		var repass = document.getElementById("repassword").value;
+
+		if (pass != repass) {
+			alert("Your password and confirmation password do not match.");
+			addnewteacher.repass.focus();
+			return false;
+		}
+
+	}
+</script>
+</head>
+<body>
+
+	<%
+		List<ClassModel> classModelList = (ArrayList<ClassModel>) request.getAttribute("classList");
+	%>
+	<form name="addnewstudent" action="addnewstudent" method="post">
+
+		<h1>Add Teacher</h1>
+
+		<fieldset>
+			<legend>
+				<span class="number">1</span>Add New Student
+			</legend>
+			<label for="name">User-Name:</label> <input type="text" id="uname"
+				name="username" required> <label for="name">Name:</label> <input
+				type="text" id="name" name="name" required> <label
+				for="regnum">Register Number:</label> <input type="text"
+				id="registernumber" name="registernumber" required> <label
+				for="password">Password:</label> <input type="password"
+				id="password" name="pass" required> <label for="password">Re-Enter
+				Password:</label> <input type="password" id="repassword" name="repass"
+				required>
+
+
+		</fieldset>
+
+
+		<fieldset>
+			<label for="job">Branch</label> <select id="branch" name="branch"
+				required>
+				<option value=""></option>
+				<option value="Computer science">Computer science</option>
+				<option value="Maths">Maths</option>
+				<option value="English">English</option>
+				<option value="Commerce">Commerce</option>
+				<option value="Visual Media">Visual Media</option>
+			</select>
+		</fieldset>
+
+
+
+
+		<button type="submit" onclick="return validate(this);">Create
+			Student</button>
+		<button type="submit">Reset</button>
+
+	</form>
+</body>
+</html>
---
+++ feedback/src/com/feedback/CreateStudent/CreateStudent.java
@@ -0,0 +1,86 @@
+package com.feedback.CreateStudent;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.feedback.database.DatabaseConnection;
+import com.feedback.model.ClassModel;
+import com.mysql.cj.Session;
+
+public class CreateStudent extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		/*List<ClassModel> classModelList = new ArrayList<>();
+
+		try {
+			Connection con = DatabaseConnection.getInstance();
+			PreparedStatement st = con.prepareStatement("select * from class");
+
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+				// fetching id and name to variables
+				int id = rs.getInt("id");
+				String name = rs.getString("className");
+
+				// Creating ClassModel Object
+				ClassModel classModel = new ClassModel();
+				classModel.setId(id);
+				classModel.setName(name);
+
+				// Adding ClassModel Object to classModelList
+				classModelList.add(classModel);
+
+			}
+
+			request.setAttribute("classList", classModelList);
+
+			request.getRequestDispatcher("/Login/Addnewstudent.jsp").forward(request, response);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}*/
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+		
+		
+		String username = request.getParameter("username").toString();
+		String name = request.getParameter("name");
+		String password = request.getParameter("pass");
+		String repassword = request.getParameter("repass");
+		String branch = request.getParameter("branch");
+		String batch = request.getParameter("model");
+		String regnum = request.getParameter("registernumber");
+
+		try {
+
+			Connection con = DatabaseConnection.getInstance();
+			PreparedStatement st = con.prepareStatement(
+					"INSERT INTO student " + "( userName, password, rollNo, name, classid, coordinatorid) "
+							+ "VALUES ( ?, ?, ?, ?, ?, ?)");
+			st.setString(1, username);
+			st.setString(2, password);
+			st.setString(3, regnum);
+			st.setString(4, name);
+			st.setString(5, batch);
+			st.setInt(6, 1);
+			st.executeUpdate();
+
+			//request.setAttribute("successMessage", "Added Student Succefully");
+			request.getRequestDispatcher("/Login/coordinatorhome.jsp").forward(request, response);
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
